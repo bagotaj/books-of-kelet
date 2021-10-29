d727e2ce86d27eaebb3770e0951222bb0103ab5c
bagotaj <bagotaj@gmail.com>
2021-10-29 20:23:25 +0200

    Changing image drawing on canvas

--- js/drawBooksImage.js
+++ js/drawBooksImage.js
@@ -2,19 +2,9 @@ function drawBooksImage() {
   const base_image = new Image();
   base_image.src = '../img/IMG_2541.jpeg';
 
-  const imgNewSizeRatio = base_image.height / canvas.height;
-
   base_image.onload = function () {
-    ctx.drawImage(
-      base_image,
-      0,
-      0,
-      base_image.width / imgNewSizeRatio,
-      base_image.height / imgNewSizeRatio
-    );
+    ctx.drawImage(base_image, 0, 0, base_image.width, base_image.height);
 
-    drawBoundingBoxes(imgNewSizeRatio);
-    makeBookTitles();
-    drawTextAnnotations(imgNewSizeRatio);
+    // drawBoundingBoxes(imgNewSizeRatio);
   };
 }
