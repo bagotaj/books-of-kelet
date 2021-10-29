cfe848bc57765c9d948c14a63204914520b7a2a4
bagotaj <bagotaj@gmail.com>
2021-10-29 20:34:48 +0200

    Creating point drawing function

---
+++ js/drawBookTitlePoint.js
@@ -0,0 +1,18 @@
+function drawBookTitlePoint(vertices) {
+  console.log(vertices);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  const pointX = vertices[0]['x'];
+  const pointY = vertices[0]['y'];
+
+  ctx.beginPath();
+  ctx.fillStyle = 'green';
+  ctx.arc(
+    pointX / imgNewSizeRatio,
+    pointY / imgNewSizeRatio,
+    5,
+    0,
+    2 * Math.PI
+  );
+  ctx.fill();
+}
