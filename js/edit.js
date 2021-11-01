37faee8ef299373164eecbe37d9f706740d4b5bd
bagotaj <bagotaj@gmail.com>
2021-11-01 22:25:47 +0100

    Adding rectangle click checker method

--- js/edit.js
+++ js/edit.js
@@ -21,18 +21,67 @@ canvas2.addEventListener(
     newCoords = getMousePosition(canvas2, event);
 
     addNewBookCoords(newCoords);
+
+    for (let i = 0; i < bookTitles.length; i++) {
+      let bookTitlesCoords = bookTitles[i]['boundingBox']['vertices'];
+
+      let inputField = document.getElementById(i);
+
+      let elementWasClicked = false;
+
+      if (bookTitlesCoords.length === 1) {
+        return;
+      } else {
+        elementWasClicked = checkBoundingBoxClicking(
+          newCoords,
+          bookTitlesCoords
+        );
+      }
+
+      if (elementWasClicked) {
+        inputField.scrollIntoView();
+      }
+    }
   },
   false
 );
 
 function getMousePosition(canvas2, event) {
-  var rect = canvas2.getBoundingClientRect();
+  let rect = canvas2.getBoundingClientRect();
   return {
     x: event.clientX - rect.left,
     y: event.clientY - rect.top,
   };
 }
 
+function checkBoundingBoxClicking(m, r) {
+  let ABP = vector(r[0], r[1], m);
+  let BCP = vector(r[1], r[2], m);
+  let CDP = vector(r[2], r[3], m);
+  let DAP = vector(r[3], r[0], m);
+
+  if (ABP > 0 && BCP > 0 && CDP > 0 && DAP > 0) {
+    return true;
+  }
+
+  return false;
+}
+
+function vector(p1, p2, m) {
+  const pointXCoord = m.x;
+  const pointYCoord = m.y;
+  const rectX1Coord = p1.x / imgNewSizeRatio;
+  const rectY1Coord = p1.y / imgNewSizeRatio;
+  const rectX2Coord = p2.x / imgNewSizeRatio;
+  const rectY2Coord = p2.y / imgNewSizeRatio;
+
+  return (
+    (pointXCoord - rectX1Coord) * (pointYCoord - rectY2Coord) -
+    (pointYCoord - rectY1Coord) * (pointXCoord - rectX2Coord)
+  );
+}
+
 const edit = true;
 
+drawBoundingBoxes(imgNewSizeRatio);
 createBookTitlesTable(edit);
