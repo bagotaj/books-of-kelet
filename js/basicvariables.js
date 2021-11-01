8a6ce17cf829a20ba4d69f56d853845a2521bce8
bagotaj <bagotaj@gmail.com>
2021-11-01 22:20:26 +0100

    Adding global variable: bookTitles

--- js/basicvariables.js
+++ js/basicvariables.js
@@ -1,3 +1,4 @@
 const blocks = db[0]['fullTextAnnotation']['pages'][0]['blocks'];
 const textAnnotations = db[0]['textAnnotations'];
+const bookTitles = getItemFromLocalStorage('bookTitles');
 const imgNewSizeRatio = 6.048;
