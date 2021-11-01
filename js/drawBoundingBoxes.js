f7354aaef9da245a7ed7a83c95324eb9f57f5978
bagotaj <bagotaj@gmail.com>
2021-11-01 22:24:33 +0100

    Adding rectangle and point drawing methods on canvas from bookTitles

--- js/drawBoundingBoxes.js
+++ js/drawBoundingBoxes.js
@@ -1,21 +1,41 @@
 function drawBoundingBoxes(imgNewSizeRatio) {
-  for (let i = 0; i < blocks.length; i++) {
-    const vertices = blocks[i]['boundingBox']['vertices'];
+  for (let i = 0; i < bookTitles.length; i++) {
+    const vertices = bookTitles[i]['boundingBox']['vertices'];
 
-    for (let j = 0; j < vertices.length; j++) {
-      const pointX = vertices[j]['x'];
-      const pointY = vertices[j]['y'];
+    const pointX = vertices[0]['x'];
+    const pointY = vertices[0]['y'];
 
-      ctx.beginPath();
-      ctx.fillStyle = 'green';
-      ctx.arc(
+    if (vertices.length === 1) {
+      ctx2.beginPath();
+      ctx2.fillStyle = 'red';
+      ctx2.arc(
         pointX / imgNewSizeRatio,
         pointY / imgNewSizeRatio,
-        1,
+        5,
         0,
         2 * Math.PI
       );
-      ctx.fill();
+      ctx2.fill();
+    } else {
+      ctx2.beginPath();
+      ctx2.fillStyle = 'green';
+      ctx2.moveTo(
+        vertices[0]['x'] / imgNewSizeRatio,
+        vertices[0]['y'] / imgNewSizeRatio
+      );
+      ctx2.lineTo(
+        vertices[1]['x'] / imgNewSizeRatio,
+        vertices[1]['y'] / imgNewSizeRatio
+      );
+      ctx2.lineTo(
+        vertices[2]['x'] / imgNewSizeRatio,
+        vertices[2]['y'] / imgNewSizeRatio
+      );
+      ctx2.lineTo(
+        vertices[3]['x'] / imgNewSizeRatio,
+        vertices[3]['y'] / imgNewSizeRatio
+      );
+      ctx2.fill();
     }
   }
 }
