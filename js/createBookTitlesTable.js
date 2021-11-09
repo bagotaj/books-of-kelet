ca4cf2eb9c9736cc942d99af0f45f9ffb69c05da
bagotaj <bagotaj@gmail.com>
2021-11-09 21:41:57 +0100

    Changing for loop parameters

--- js/createBookTitlesTable.js
+++ js/createBookTitlesTable.js
@@ -13,6 +13,7 @@ function createBookTitlesTable(edit) {
         let input = document.createElement('input');
         input.id = i;
         input.value = text;
+
         let buttonSave = document.createElement('button');
         buttonSave.innerText = 'Mentés';
         buttonSave.addEventListener('click', () => dataManipulation('save', i));
@@ -24,7 +25,7 @@ function createBookTitlesTable(edit) {
 
         const fieldOfColumns = [input, buttonSave, buttonDelete];
 
-        for (let i = 0; i < 3; i++) {
+        for (let i = 0; i < fieldOfColumns.length; i++) {
           let td = document.createElement('td');
 
           td.appendChild(fieldOfColumns[i]);
