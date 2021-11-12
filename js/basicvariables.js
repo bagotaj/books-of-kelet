1ee161c6961110bee425fcaabfb892c34e494138
bagotaj <bagotaj@gmail.com>
2021-11-12 19:21:14 +0100

    Adding new book title image size ratio

--- js/basicvariables.js
+++ js/basicvariables.js
@@ -2,3 +2,4 @@ const blocks = db[0]['fullTextAnnotation']['pages'][0]['blocks'];
 const textAnnotations = db[0]['textAnnotations'];
 const bookTitles = getItemFromLocalStorage('bookTitles');
 const imgNewSizeRatio = 6.048;
+const imgSizeRatioBookTitle = 7.56;
