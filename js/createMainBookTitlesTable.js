9e435d580c40b4775aa6fc3d3901ac56ac00b09b
bagotaj <bagotaj@gmail.com>
2021-11-16 16:20:57 +0100

    Changing property name (imageTitle) and adding book title sorting function

--- js/createMainBookTitlesTable.js
+++ js/createMainBookTitlesTable.js
@@ -1,8 +1,21 @@
 function createMainBookTitlesTable() {
   const tableBody = document.getElementById('tableBody');
 
-  for (const bookTitlesOnImage in dbbooks) {
-    const bookTitlesData = dbbooks[bookTitlesOnImage]['bookdata'];
+  for (const imageTitle in dbbooks) {
+    const bookTitlesData = dbbooks[imageTitle]['bookdata'];
+
+    bookTitlesData.sort(function (a, b) {
+      var titleA = a.paragraph.toUpperCase();
+      var titleB = b.paragraph.toUpperCase();
+      if (titleA < titleB) {
+        return -1;
+      }
+      if (titleA > titleB) {
+        return 1;
+      }
+
+      return 0;
+    });
 
     for (let i = 0; i < bookTitlesData.length; i++) {
       if (bookTitlesData[i]['display'] === 'on') {
@@ -18,7 +31,7 @@ function createMainBookTitlesTable() {
         let p = document.createElement('p');
         p.innerText = text;
         p.addEventListener('click', () => {
-          showBookshelves(bookTitlesOnImage);
+          showBookshelves(imageTitle);
           drawBookTitlePoint(bookTitlePointCoords);
           canvasTop.scrollIntoView({
             behavior: 'smooth',
