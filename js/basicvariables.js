324ee73082fead01b892649928734d7b26079595
bagotaj <bagotaj@gmail.com>
2021-11-17 20:43:54 +0100

    Declaring the bookTitles variable and listOfImages array

--- js/basicvariables.js
+++ js/basicvariables.js
@@ -1,5 +1,7 @@
 const blocks = db[0]['fullTextAnnotation']['pages'][0]['blocks'];
 const textAnnotations = db[0]['textAnnotations'];
-const bookTitles = getItemFromLocalStorage('bookTitles');
+// const bookTitles = getItemFromLocalStorage('bookTitles');
+let bookTitles;
 const imgNewSizeRatio = 6.048;
 const imgSizeRatioBookTitle = 7.56;
+const listOfImages = ['IMG_3365', 'IMG_3366', 'IMG_3367', 'IMG_3402'];
