9bb7ceff2b61a5f0329a2bba2db66870d02cd1cf
bagotaj <bagotaj@gmail.com>
2021-11-25 18:08:01 +0100

    Adding new image to the array

--- js/basicvariables.js
+++ js/basicvariables.js
@@ -4,4 +4,10 @@ const textAnnotations = db[0]['textAnnotations'];
 let bookTitles;
 const imgNewSizeRatio = 6.048;
 const imgSizeRatioBookTitle = 7.56;
-const listOfImages = ['IMG_3365', 'IMG_3366', 'IMG_3367', 'IMG_3424'];
+const listOfImages = [
+  'IMG_3365',
+  'IMG_3366',
+  'IMG_3367',
+  'IMG_3424',
+  'IMG_3425',
+];
