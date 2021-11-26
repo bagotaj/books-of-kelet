deb632d30cafecbdc2f28f5d0eb146119212f3f6
bagotaj <bagotaj@gmail.com>
2021-11-26 19:53:00 +0100

    Adding tfoot and pagination function

--- js/makePagination.js
+++ js/makePagination.js
@@ -1,12 +1,44 @@
 function makePagination() {
-  const numberOfBookTitles = makeSortedBookTitles();
-  const numberPerPage = 50;
-  const currentPage = 1;
-  const numberOfPages = Math.ceil(numberOfBookTitles / numberPerPage);
+  const tfoot = document.querySelector('#tableFooter');
+
+  const tr = document.createElement('tr');
+  const td = document.createElement('td');
+
+  let numberOfBookTitles = makeSortedBookTitles();
+
+  const numberOfPages = Math.ceil(numberOfBookTitles.length / numberPerPage);
+
+  for (let i = 0; i < numberOfPages; i++) {
+    const btn = document.createElement('button');
+    btn.value = i + 1;
+    btn.innerText = i + 1;
+
+    if (i === 0) {
+      btn.classList.add('button-clicked');
+    }
+
+    btn.addEventListener('click', (e) => {
+      td.getElementsByClassName('button-clicked')[0].classList.remove(
+        'button-clicked'
+      );
+      btn.classList.add('button-clicked');
+      createMainBookTitlesTable(e.target.value);
+    });
+
+    td.appendChild(btn);
+  }
+
+  tr.appendChild(td);
+  tfoot.appendChild(tr);
 }
 
-function buildPage(currentPage) {
+function getSortedBookTitles(currentPage) {
+  let numberOfBookTitles = makeSortedBookTitles();
+  let paginatedBookTitles;
+
   const trimStart = (currentPage - 1) * numberPerPage;
   const trimEnd = trimStart + numberPerPage;
-  console.log(listArray.slice(trimStart, trimEnd));
+  paginatedBookTitles = numberOfBookTitles.slice(trimStart, trimEnd);
+
+  return paginatedBookTitles;
 }
