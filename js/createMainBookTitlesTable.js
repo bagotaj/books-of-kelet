78e6aa641efc859354b0373c202d5ff0c20342b2
bagotaj <bagotaj@gmail.com>
2021-11-26 19:47:08 +0100

    Adding pagination to main book title table creator function

--- js/createMainBookTitlesTable.js
+++ js/createMainBookTitlesTable.js
@@ -1,36 +1,33 @@
-function createMainBookTitlesTable() {
+function createMainBookTitlesTable(currentPage = 1) {
   const tableBody = document.getElementById('tableBody');
-  const sortedBookTitles = makeSortedBookTitles();
+  tableBody.innerHTML = '';
+
+  const sortedBookTitles = getSortedBookTitles(currentPage);
 
   for (let i = 0; i < sortedBookTitles.length; i++) {
-    if (sortedBookTitles[i]['display'] === 'on') {
-      const bookTitlePointCoords =
-        sortedBookTitles[i]['boundingBox']['vertices'];
-      const text = sortedBookTitles[i]['paragraph'];
+    const bookTitlePointCoords = sortedBookTitles[i]['boundingBox']['vertices'];
+    const text = sortedBookTitles[i]['paragraph'];
 
-      let td = document.createElement('td');
-      let tr = document.createElement('tr');
+    let td = document.createElement('td');
+    let tr = document.createElement('tr');
 
-      const canvasTop = document.getElementById('canvas');
+    const canvasTop = document.getElementById('canvas');
 
-      let p = document.createElement('p');
-      p.innerText = text;
-      p.addEventListener('click', () => {
-        showBookshelves(
-          sortedBookTitles[i]['imageTitle'],
-          sortedBookTitles[i]['shelfName']
-        );
-        drawBookTitlePoint(bookTitlePointCoords);
-        canvasTop.scrollIntoView({
-          behavior: 'smooth',
-        });
+    let p = document.createElement('p');
+    p.innerText = text;
+    p.addEventListener('click', () => {
+      showBookshelves(
+        sortedBookTitles[i]['imageTitle'],
+        sortedBookTitles[i]['shelfName']
+      );
+      drawBookTitlePoint(bookTitlePointCoords);
+      canvasTop.scrollIntoView({
+        behavior: 'smooth',
       });
-      td.appendChild(p);
-      tr.appendChild(td);
+    });
+    td.appendChild(p);
+    tr.appendChild(td);
 
-      tableBody.appendChild(tr);
-    } else {
-      continue;
-    }
+    tableBody.appendChild(tr);
   }
 }
