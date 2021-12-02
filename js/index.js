04e7e82bdf709d1639a4330bce024df46e0fa193
bagotaj <bagotaj@gmail.com>
2021-12-02 16:30:47 +0100

    Making setCanvasWrapperIndex canvas resizing function

--- js/index.js
+++ js/index.js
@@ -1,18 +1,26 @@
 function createCanvas() {
   const result = document.getElementById('canvas');
   result.width = window.innerWidth;
-  if (window.innerHeight < 500) {
-    result.height = 100;
-  } else {
-    result.height = 500;
-  }
+  setCanvasWrapperIndex(result);
+  // if (window.innerHeight < 500) {
+  //   result.height = 100;
+  //   imgNewSizeRatio = 6.048 * 5;
+  //   imgSizeRatioBookTitle = 7.56 * 5;
+  // } else {
+  //   result.height = 500;
+  //   imgNewSizeRatio = 6.048;
+  //   imgSizeRatioBookTitle = 7.56;
+  // }
   return result;
 }
 
-function followWindowSize(canvas, window) {
-  canvas.width = result.width;
-  canvas.height = result.height;
-}
+window.addEventListener('resize', function () {
+  canvas.width = window.innerWidth;
+
+  if (window.innerWidth < backgroundShelves) {
+    setCanvasWrapperIndex(canvas);
+  }
+});
 
 const canvas = createCanvas();
 const ctx = canvas.getContext('2d');
@@ -20,3 +28,14 @@ const ctx = canvas.getContext('2d');
 createMainBookTitlesTable();
 createABCLinkButtons();
 makePagination();
+
+function setCanvasWrapperIndex(canvas) {
+  const canvasWrapperIndex = document.querySelectorAll('.canvas-wrapper-index');
+  let canvasHeight = (window.innerWidth * 500) / backgroundShelves;
+
+  canvas.height = canvasHeight;
+
+  canvasWrapperIndex[0].style.height = canvasHeight;
+  canvasWrapperIndex[0].style.backgroundImage =
+    "url('../img/kelet-header.jpeg')";
+}
