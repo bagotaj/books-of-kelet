91e222912dba19f06373727df1b08d65e07c57df
bagotaj <bagotaj@gmail.com>
2022-03-27 16:09:48 +0200

    Deleted files

--- js/localStorageItems.js
+++
@@ -1,9 +0,0 @@
-function getItemFromLocalStorage(keyName) {
-  const data = JSON.parse(localStorage.getItem(keyName));
-
-  return data;
-}
-
-function saveItemToLocalStorage(keyName, keyValue) {
-  localStorage.setItem(keyName, JSON.stringify(keyValue));
-}
--- js/makeSortedBookTitlesByABC.js
+++
@@ -1,9 +0,0 @@
-function makeSortedBookTitlesByABC(letter = 'A') {
-  let sortedBookTitlesByABC;
-
-  sortedBookTitlesByABC = bookTitles.filter(
-    (bookTitle) => bookTitle['paragraph'].trim()[0] === letter
-  );
-
-  return sortedBookTitlesByABC;
-}
