fc17380d31377001e59c15faeced703e18e0399b
bagotaj <bagotaj@gmail.com>
2022-03-29 14:12:47 +0200

    Making a firestore data downloader

---
+++ src/getDB.js
@@ -0,0 +1,23 @@
+const db = firebase.firestore();
+
+db.collection('test').onSnapshot((querySnapshot) => {
+  const results = [];
+
+  querySnapshot.forEach((doc) => {
+    results.push(doc.data());
+  });
+  makeListFromData(results);
+});
+
+function makeListFromData(results) {
+  results.forEach((value) => {
+    Object.values(value).map((element) => {
+      const loggedinContainerContentData =
+        document.querySelector('.contentData');
+      const p = document.createElement('p');
+
+      p.textContent = element + ',';
+      loggedinContainerContentData.appendChild(p);
+    });
+  });
+}
