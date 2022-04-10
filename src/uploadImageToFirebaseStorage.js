32ebce5401384eb562ee58b9c4829363fece02e8
bagotaj <bagotaj@gmail.com>
2022-04-10 17:09:45 +0200

    Making image uploader and controller functions

---
+++ src/uploadImageToFirebaseStorage.js
@@ -0,0 +1,101 @@
+// Source https://youtu.be/ZH-PnY-JGBU
+
+const dbconnection = firebase.firestore();
+
+let ImgName, ImgUrl;
+let files = [];
+let reader;
+
+// Select button
+let selectButton = document.querySelector('#selectButton');
+selectButton.addEventListener('click', (e) => {
+  let input = document.createElement('input');
+  input.type = 'file';
+
+  input.onchange = (e) => {
+    files = e.target.files;
+    reader = new FileReader();
+    reader.onload = function () {
+      // show chosen image on the site
+      document.getElementById('myimg').src = reader.result;
+    };
+    reader.readAsDataURL(files[0]);
+  };
+  input.click();
+});
+
+// Upload button
+let uploadButton = document.querySelector('#uploadButton');
+uploadButton.addEventListener('click', () => {
+  // Namebox - give the name to the image file
+  ImgName = document.getElementById('namebox').value;
+  let uploadTask = firebase
+    .storage()
+    .ref(`assets/img/${ImgName}.jpeg`)
+    .put(files[0]);
+
+  uploadTask.on(
+    'state_changed',
+    (snapshot) => {
+      let progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      document.getElementById('UpProgress').innerText = `Upload ${progress}%`;
+    },
+
+    // error handling
+    (error) => {
+      alert('error in saving the image');
+    },
+
+    // submit image link to database
+    () => {
+      uploadTask.snapshot.ref.getDownloadURL().then((url) => {
+        ImgUrl = url;
+
+        dbconnection
+          .collection('images')
+          .add({
+            Name: ImgName,
+            Link: ImgUrl,
+          })
+          .then(() => {
+            console.log('Document written');
+          })
+          .catch((error) => {
+            console.error('Error adding document: ', error);
+          });
+
+        // firebase.auth().database().ref(`images/${ImgName}`).set({
+        //       Name: ImgName,
+        //       Link: ImgUrl,
+        //     });
+      });
+
+      alert('image added successfully');
+    }
+  );
+});
+
+// Retrieval button
+let retrievalButton = document.querySelector('#retrievalButton');
+retrievalButton.addEventListener('click', () => {
+  ImgName = document.getElementById('namebox').value;
+
+  // NEM JO!!!
+  dbconnection.collection('images').onSnapshot((querySnapshot) => {
+    const results = [];
+
+    querySnapshot.forEach((doc) => {
+      results.push(doc.data());
+    });
+    makeListFromData(results);
+  });
+
+  // instead firebase.firestore() ?
+  // firebase
+  //   .database()
+  //   .ref(`images/${ImgName}`)
+  //   .on('value', (snapshot) => {
+  //     // show downloaded image on the site
+  //     document.getElementById('myimg').src = snapshot.val().Link;
+  //   });
+});
