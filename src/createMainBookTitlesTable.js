e8aa6f2be506f3123aa9862d485a8ed9d6b5a104
bagotaj <bagotaj@gmail.com>
2022-04-22 21:11:56 +0200

    Changing function argument and adding display "on" checker

--- src/createMainBookTitlesTable.js
+++ src/createMainBookTitlesTable.js
@@ -1,42 +1,44 @@
-function createMainBookTitlesTable(currentPage = 1) {
+function createMainBookTitlesTable(books) {
   const tableBody = document.getElementById('tableBody');
   tableBody.innerHTML = '';
 
-  const sortedBookTitles = getSortedBookTitlesByCurrentPage(currentPage);
+  // const sortedBookTitles = getSortedBookTitlesByCurrentPage(currentPage);
 
-  for (let i = 0; i < sortedBookTitles.length; i++) {
-    const bookTitlePointCoords = sortedBookTitles[i]['boundingBox']['vertices'];
-    const text = sortedBookTitles[i]['paragraph'];
+  for (let i = 0; i < books.length; i++) {
+    if (books[i]['display'] === 'on') {
+      const bookTitlePointCoords = books[i]['boundingBox']['vertices'];
+      const text = books[i]['paragraph'];
 
-    let td = document.createElement('td');
-    let td2 = document.createElement('td');
-    let tr = document.createElement('tr');
+      let td = document.createElement('td');
+      let td2 = document.createElement('td');
+      let tr = document.createElement('tr');
 
-    let p = document.createElement('p');
-    p.innerText = text;
-    p.addEventListener('click', () => {
-      showBookshelves(
-        sortedBookTitles[i]['imageTitle'],
-        sortedBookTitles[i]['shelfName'],
-        sortedBookTitles[i]['shelfCoords']
-      );
-      drawBookTitlePoint(bookTitlePointCoords);
-      canvasTop.scrollIntoView({
-        behavior: 'smooth',
+      let p = document.createElement('p');
+      p.innerText = text;
+      p.addEventListener('click', () => {
+        showBookshelves(
+          books[i]['imageTitle'],
+          books[i]['shelfName'],
+          books[i]['shelfCoords']
+        );
+        drawBookTitlePoint(bookTitlePointCoords);
+        canvasTop.scrollIntoView({
+          behavior: 'smooth',
+        });
       });
-    });
-    td.appendChild(p);
-    tr.appendChild(td);
+      td.appendChild(p);
+      tr.appendChild(td);
 
-    const a = document.createElement('a');
+      const a = document.createElement('a');
 
-    a.innerText = 'moly.hu';
-    a.href = `https://moly.hu/kereses?q=${text}`;
-    a.target = '_blank';
+      a.innerText = 'moly.hu';
+      a.href = `https://moly.hu/kereses?q=${text}`;
+      a.target = '_blank';
 
-    td2.appendChild(a);
-    tr.appendChild(td2);
+      td2.appendChild(a);
+      tr.appendChild(td2);
 
-    tableBody.appendChild(tr);
+      tableBody.appendChild(tr);
+    }
   }
 }
