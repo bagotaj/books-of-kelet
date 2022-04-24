7a5b2a999e3a75f1c17fffc331efae623bc85b99
bagotaj <bagotaj@gmail.com>
2022-04-24 18:41:49 +0200

    Making data downloader and data renderer function

--- src/getDB.js
+++ src/getDB.js
@@ -1,23 +1,28 @@
-function getDBfromDatabase() {
-  dbconnection.collection('test').onSnapshot((querySnapshot) => {
-    const results = [];
+function getBooksByABC(letterStart, letterEnd) {
+  let booksRef = dbconnection
+    .collection('books')
+    .orderBy('paragraph')
+    .startAt(letterStart)
+    .endAt(letterEnd);
 
-    querySnapshot.forEach((doc) => {
-      results.push(doc.data());
+  booksRef
+    .get()
+    .then((querySnapshot) => {
+      let books = [];
+
+      querySnapshot.forEach((book) => {
+        books.push(book.data());
+      });
+
+      return books;
+    })
+    .then((books) => {
+      createMainBookTitlesTable(books);
+      createABCLinkButtons();
+      makePagination(books);
+    })
+    .catch((error) => {
+      console.error('Error adding document: ', error);
+      // alert('Error adding document: ', error.message);
     });
-    makeListFromData(results);
-  });
-}
-
-function makeListFromData(results) {
-  results.forEach((value) => {
-    Object.values(value).map((element) => {
-      const loggedinContainerContentData =
-        document.querySelector('.contentData');
-      const p = document.createElement('p');
-
-      p.textContent = element + ',';
-      loggedinContainerContentData.appendChild(p);
-    });
-  });
 }
