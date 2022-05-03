90791db8a775557c55ef19bc06c9e7363ba060d5
bagotaj <bagotaj@gmail.com>
2022-05-03 19:47:34 +0200

    Making condition. If sortedBookTitlesByABC variable is defined the pagination is happened'

--- src/makePagination.js
+++ src/makePagination.js
@@ -5,30 +5,34 @@ function makePagination(sortedBookTitlesByABC) {
   const tr = document.createElement('tr');
   const td = document.createElement('td');
 
-  const numberOfPages = Math.ceil(sortedBookTitlesByABC.length / numberPerPage);
+  if (sortedBookTitlesByABC !== undefined) {
+    const numberOfPages = Math.ceil(
+      sortedBookTitlesByABC.length / numberPerPage
+    );
 
-  for (let i = 0; i < numberOfPages; i++) {
-    const btn = document.createElement('button');
-    btn.value = i + 1;
-    btn.innerText = i + 1;
+    for (let i = 0; i < numberOfPages; i++) {
+      const btn = document.createElement('button');
+      btn.value = i + 1;
+      btn.innerText = i + 1;
 
-    if (i === 0) {
-      btn.classList.add('button-clicked');
+      if (i === 0) {
+        btn.classList.add('button-clicked');
+      }
+
+      btn.addEventListener('click', (e) => {
+        td.getElementsByClassName('button-clicked')[0].classList.remove(
+          'button-clicked'
+        );
+        btn.classList.add('button-clicked');
+        createMainBookTitlesTable(e.target.value);
+      });
+
+      td.appendChild(btn);
     }
 
-    btn.addEventListener('click', (e) => {
-      td.getElementsByClassName('button-clicked')[0].classList.remove(
-        'button-clicked'
-      );
-      btn.classList.add('button-clicked');
-      createMainBookTitlesTable(e.target.value);
-    });
-
-    td.appendChild(btn);
+    tr.appendChild(td);
+    tfoot.appendChild(tr);
   }
-
-  tr.appendChild(td);
-  tfoot.appendChild(tr);
 }
 
 function getSortedBookTitlesByCurrentPage(currentPage) {
