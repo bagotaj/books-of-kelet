c036fd4a070c53084b68b364c1063b05f2da9756
bagotaj <bagotaj@gmail.com>
2022-05-07 20:08:58 +0200

    Modifying data upload function because batch didn't work

--- public/src/createFirebaseDB.js
+++ public/src/createFirebaseDB.js
@@ -1,22 +1,48 @@
 function createFirebaseDBBooks() {
   let newDB = { 1: [], 2: [], 3: [] };
+  let newDB2 = [];
 
   for (let key in dbbooks) {
     let bookdataArr = dbbooks[key].bookdata;
 
     bookdataArr.forEach((book) => {
-      if (newDB['1'].length === 500) {
-        if (newDB['2'].length === 500) {
-          newDB['3'].push(book);
-        } else {
-          newDB['2'].push(book);
-        }
-      } else {
-        newDB['1'].push(book);
-      }
+      book['imageTitle'] = dbbooks[key]['imageName'];
+      book['shelfName'] = key;
+      book['shelfCoords'] = {
+        x: dbbooks[key]['shelfcoords']['x'],
+        y: dbbooks[key]['shelfcoords']['y'],
+      };
+
+      newDB2.push(book);
     });
   }
 
+  newDB2.sort(function (a, b) {
+    var titleA = a.paragraph.toUpperCase();
+    var titleB = b.paragraph.toUpperCase();
+    if (titleA < titleB) {
+      return -1;
+    }
+    if (titleA > titleB) {
+      return 1;
+    }
+
+    return 0;
+  });
+
+  newDB2.forEach((element) => {
+    if (newDB['1'].length === 450) {
+      if (newDB['2'].length === 450) {
+        newDB['3'].push(element);
+      } else {
+        newDB['2'].push(element);
+      }
+    } else {
+      newDB['1'].push(element);
+    }
+  });
+
+  // console.log('Ordered Books', newDB);
   return newDB;
 }
 
