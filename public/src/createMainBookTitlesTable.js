a878d6289bef4daaab410416b5ae0d867eac071f
bagotaj <bagotaj@gmail.com>
2022-05-18 17:50:32 +0200

    Adding new p element with link part: svg and moly.hu label

--- public/src/createMainBookTitlesTable.js
+++ public/src/createMainBookTitlesTable.js
@@ -20,6 +20,7 @@ function createMainBookTitlesTable(books) {
       let tr = document.createElement('tr');
 
       let p = document.createElement('p');
+      let p2 = document.createElement('p');
       p.innerText = text;
       p.addEventListener('click', () => {
         showBookshelves(
@@ -37,11 +38,21 @@ function createMainBookTitlesTable(books) {
 
       const a = document.createElement('a');
 
-      a.innerText = 'Keresés a moly.hu-n';
+      a.innerHTML = `<svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      fill="currentColor"
+      class="bi bi-search"
+      viewBox="0 0 16 16"
+    >
+      <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+    </svg> moly.hu`;
       a.href = `https://moly.hu/kereses?q=${text}`;
       a.target = '_blank';
 
-      td2.appendChild(a);
+      p2.appendChild(a);
+      td2.appendChild(p2);
       tr.appendChild(td2);
 
       tableBody.appendChild(tr);
