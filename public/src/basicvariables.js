3eb7b2a4ee771db42dabf377b86c51cd1f7593df
bagotaj <bagotaj@gmail.com>
2022-05-22 18:51:07 +0200

    Adding column to listOfImages array

--- public/src/basicvariables.js
+++ public/src/basicvariables.js
@@ -8,5 +8,6 @@ const listOfImages = [
   { column: 4, images: ['IMG_3424', 'IMG_3425', 'IMG_3426'] },
   { column: 5, images: ['IMG_3421', 'IMG_3422', 'IMG_3423'] },
   { column: 6, images: ['IMG_3414', 'IMG_3415', 'IMG_3427', 'IMG_3428'] },
+  { column: 7, images: ['IMG_3785'] },
 ];
 const numberPerPage = 50;
