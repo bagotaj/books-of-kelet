f63e1134c93c8ebf241aa2d3c3c7cfeff01be6e1
bagotaj <bagotaj@gmail.com>
2022-05-27 18:07:04 +0200

    Adding new images to column 8

--- public/src/basicvariables.js
+++ public/src/basicvariables.js
@@ -9,5 +9,6 @@ const listOfImages = [
   { column: 5, images: ['IMG_3421', 'IMG_3422', 'IMG_3423'] },
   { column: 6, images: ['IMG_3414', 'IMG_3415', 'IMG_3427', 'IMG_3428'] },
   { column: 7, images: ['IMG_3785', 'IMG_3787'] },
+  { column: 8, images: ['IMG_3788', 'IMG_3789'] },
 ];
 const numberPerPage = 50;
