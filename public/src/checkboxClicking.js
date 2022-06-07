5a2171a6eedd570f58e64e96c58579f7097b1b02
bagotaj <bagotaj@gmail.com>
2022-06-07 18:32:31 +0200

    Making a separate checkboxClicking js file to functions

---
+++ public/src/checkboxClicking.js
@@ -0,0 +1,30 @@
+function checkBoxClicking(mouseVector, rectangleVectors, basicSet) {
+  if (basicSet) {
+    imgNewSizeRatio = 0;
+  }
+
+  let ABP = vector(rectangleVectors[0], rectangleVectors[1], mouseVector);
+  let BCP = vector(rectangleVectors[1], rectangleVectors[2], mouseVector);
+  let CDP = vector(rectangleVectors[2], rectangleVectors[3], mouseVector);
+  let DAP = vector(rectangleVectors[3], rectangleVectors[0], mouseVector);
+
+  if (ABP > 0 && BCP > 0 && CDP > 0 && DAP > 0) {
+    return true;
+  }
+
+  return false;
+}
+
+function vector(p1, p2, mouseVector) {
+  const pointXCoord = mouseVector.x;
+  const pointYCoord = mouseVector.y;
+  const rectX1Coord = p1.x / imgNewSizeRatio;
+  const rectY1Coord = p1.y / imgNewSizeRatio;
+  const rectX2Coord = p2.x / imgNewSizeRatio;
+  const rectY2Coord = p2.y / imgNewSizeRatio;
+
+  return (
+    (pointXCoord - rectX1Coord) * (pointYCoord - rectY2Coord) -
+    (pointYCoord - rectY1Coord) * (pointXCoord - rectX2Coord)
+  );
+}
