f1d11a40acae0217fc6a62ad13640eeb49725df8
bagotaj <bagotaj@gmail.com>
2022-06-13 20:32:09 +0200

    Moving two functions to a separate file

--- public/src/index.js
+++ public/src/index.js
@@ -24,27 +24,6 @@ function initializeSite() {
   });
 }
 
-function setCanvasHeightIndex() {
-  let canvasHeight =
-    (window.innerWidth * backgroundShelvesHeight) / backgroundShelvesWidth;
-
-  return canvasHeight;
-}
-
-function setCanvasWrapperIndex(canvas, imageURL) {
-  const canvasWrapperIndexClass = document.querySelectorAll(
-    '.canvas-wrapper-index'
-  );
-  let canvasHeight = setCanvasHeightIndex();
-
-  canvas.height = canvasHeight;
-  imgSizeRatioBookTitle =
-    7.56 / ((canvasHeight * 0.8) / (backgroundShelvesHeight * 0.8));
-  shelvesCoordsRatio = canvasHeight / backgroundShelvesHeight;
-  canvasWrapperIndexClass[0].style.height = canvasHeight;
-  canvasWrapperIndexClass[0].style.backgroundImage = `url(${imageURL})`;
-}
-
 function setBookShelvesClassIndex() {
   const bookShelvesClass = document.querySelectorAll('.book-shelves');
 
