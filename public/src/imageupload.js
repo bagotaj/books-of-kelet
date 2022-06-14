9860c9bd1a19e5237cc726cf3b06efc88fb48983
bagotaj <bagotaj@gmail.com>
2022-06-14 19:42:02 +0200

    Removing setCanvasHeightIndex and setCanvasWrapperIndex function to a separate file. Deleting unused setCanvasWrapperIndex function calling.

--- public/src/imageupload.js
+++ public/src/imageupload.js
@@ -1,7 +1,9 @@
+let setClick = 'grid';
+
 function createCanvas() {
   const result = document.getElementById('imageuploadcanvas');
   result.width = window.innerWidth;
-  setCanvasWrapperIndex(result);
+  getImage(result, backgroundShelvesTitle);
 
   return result;
 }
@@ -10,7 +12,7 @@ window.addEventListener('resize', function () {
   imageuploadcanvas.width = window.innerWidth;
 
   if (window.innerWidth < backgroundShelvesWidth) {
-    setCanvasWrapperIndex(imageuploadcanvas);
+    getImage(imageuploadcanvas, backgroundShelvesTitle);
   }
 });
 
@@ -22,32 +24,14 @@ imageuploadcanvas.addEventListener(
   function (event) {
     let newCoords = getMousePosition(imageuploadcanvas, event);
 
-    drawGrid(newCoords);
+    if (setClick === 'grid') {
+      drawGrid(newCoords);
+    }
+
+    if (setClick === 'uploadimage') {
+      // uploadImageToFirebaseStorage - call a function there
+      // drawGrid(newCoords);
+    }
   },
   false
 );
-
-function setCanvasHeightIndex() {
-  let canvasHeight =
-    (window.innerWidth * backgroundShelvesHeight) / backgroundShelvesWidth;
-
-  return canvasHeight;
-}
-
-function setCanvasWrapperIndex(imageuploadcanvas) {
-  const canvasWrapperIndexClass = document.querySelectorAll(
-    '.canvas-wrapper-index'
-  );
-  let canvasHeight = setCanvasHeightIndex();
-
-  imageuploadcanvas.height = canvasHeight;
-  imgSizeRatioBookTitle =
-    7.56 / ((canvasHeight * 0.8) / (backgroundShelvesHeight * 0.8));
-  shelvesCoordsRatio = canvasHeight / backgroundShelvesHeight;
-  canvasWrapperIndexClass[0].style.height = canvasHeight;
-  canvasWrapperIndexClass[0].style.backgroundImage =
-    "url('./assets/img/kelet-header.jpeg')";
-
-  gridXCoords.push(0, imageuploadcanvas.width);
-  gridYCoords.push(0, imageuploadcanvas.height);
-}
