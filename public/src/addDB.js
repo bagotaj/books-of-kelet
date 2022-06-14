17cec97b8158536c21f981be3082c993bc2866e7
bagotaj <bagotaj@gmail.com>
2022-06-14 20:04:25 +0200

    Making addShelfBoxCoordsToFirestore function

---
+++ public/src/addDB.js
@@ -0,0 +1,18 @@
+function addShelfBoxCoordsToFirestore(shelfBoxSendingDataObj) {
+  let basicShelf = dbconnection
+    .collection('basics')
+    .where('basicShelfTitle', '==', shelfBoxSendingDataObj.shelfTitle);
+
+  basicShelf.get().then((querySnapshot) => {
+    querySnapshot.forEach((shelf) => {
+      if (!shelf.exists) {
+        throw 'Document does not exist!';
+      }
+
+      dbconnection.collection('basics').doc(shelf.id).update({
+        shelfBoxCoords: shelfBoxSendingDataObj.shelfBoxCoords,
+        shelfGridCoords: shelfBoxSendingDataObj.shelfGridCoords,
+      });
+    });
+  });
+}
