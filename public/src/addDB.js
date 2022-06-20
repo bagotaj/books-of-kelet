54e0e3ec6560ce957f9c0e41cfef1420cdc7961f
bagotaj <bagotaj@gmail.com>
2022-06-20 21:08:49 +0200

    Disabling data upload and adding console log

--- public/src/addDB.js
+++ public/src/addDB.js
@@ -1,18 +1,20 @@
 function addShelfBoxCoordsToFirestore(shelfBoxSendingDataObj) {
-  let basicShelf = dbconnection
-    .collection('basics')
-    .where('basicShelfTitle', '==', shelfBoxSendingDataObj.shelfTitle);
+  console.log(shelfBoxSendingDataObj);
 
-  basicShelf.get().then((querySnapshot) => {
-    querySnapshot.forEach((shelf) => {
-      if (!shelf.exists) {
-        throw 'Document does not exist!';
-      }
+  // let basicShelf = dbconnection
+  //   .collection('basics')
+  //   .where('basicShelfTitle', '==', shelfBoxSendingDataObj.shelfTitle);
 
-      dbconnection.collection('basics').doc(shelf.id).update({
-        shelfBoxCoords: shelfBoxSendingDataObj.shelfBoxCoords,
-        shelfGridCoords: shelfBoxSendingDataObj.shelfGridCoords,
-      });
-    });
-  });
+  // basicShelf.get().then((querySnapshot) => {
+  //   querySnapshot.forEach((shelf) => {
+  //     if (!shelf.exists) {
+  //       throw 'Document does not exist!';
+  //     }
+
+  //     dbconnection.collection('basics').doc(shelf.id).update({
+  //       shelfBoxCoords: shelfBoxSendingDataObj.shelfBoxCoords,
+  //       shelfGridCoords: shelfBoxSendingDataObj.shelfGridCoords,
+  //     });
+  //   });
+  // });
 }
