f262c86e6406d7be9fd4ee75d77f0dbc4d8059bd
bagotaj <bagotaj@gmail.com>
2022-06-20 18:06:47 +0200

    Moving some frequently used selected DOM elements to a separate file

---
+++ public/src/selectedDOMElements.js
@@ -0,0 +1,9 @@
+let canvasImageUpload = document.querySelector('#canvasImageUpload');
+let showingImageImageUpload = document.querySelector(
+  '#showingImageImageUpload'
+);
+let startPageButtons = document.querySelector('#startPageButtons');
+let addNewShelfImageButtons = document.querySelector(
+  '#addNewShelfImageButtons'
+);
+let saveNewShelfButtons = document.querySelector('#saveNewShelfButtons');
