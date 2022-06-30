7209b17610a2d91b16359a4f0a944ad098e64a55
bagotaj <bagotaj@gmail.com>
2022-06-30 19:51:28 +0200

    Making addBasicsToFirestore function to upload basic shelf data to database

--- public/src/addDB.js
+++ public/src/addDB.js
@@ -1,20 +1,41 @@
+function addBasicsToFirestore(dataOfImageToFirebase) {
+  let basicShelfTitle = dataOfImageToFirebase.basicShelfTitle;
+  let basicShelfParams = {
+    width: dataOfImageToFirebase.basicShelfParams.width,
+    height: dataOfImageToFirebase.basicShelfParams.height,
+  };
+
+  dbconnection
+    .collection('basics')
+    .doc()
+    .set({ basicShelfTitle, basicShelfParams })
+    .catch((error) => {
+      console.error('Error adding document: ', error);
+      // alert('Error adding document: ', error.message);
+    });
+}
+
 function addShelfBoxCoordsToFirestore(shelfBoxSendingDataObj) {
-  console.log(shelfBoxSendingDataObj);
+  let basicShelf = dbconnection
+    .collection('basics')
+    .where('basicShelfTitle', '==', shelfBoxSendingDataObj.shelfTitle);
 
-  // let basicShelf = dbconnection
-  //   .collection('basics')
-  //   .where('basicShelfTitle', '==', shelfBoxSendingDataObj.shelfTitle);
+  basicShelf
+    .get()
+    .then((querySnapshot) => {
+      querySnapshot.forEach((shelf) => {
+        if (!shelf.exists) {
+          throw 'Document does not exist!';
+        }
 
-  // basicShelf.get().then((querySnapshot) => {
-  //   querySnapshot.forEach((shelf) => {
-  //     if (!shelf.exists) {
-  //       throw 'Document does not exist!';
-  //     }
-
-  //     dbconnection.collection('basics').doc(shelf.id).update({
-  //       shelfBoxCoords: shelfBoxSendingDataObj.shelfBoxCoords,
-  //       shelfGridCoords: shelfBoxSendingDataObj.shelfGridCoords,
-  //     });
-  //   });
-  // });
+        dbconnection.collection('basics').doc(shelf.id).update({
+          shelfBoxCoords: shelfBoxSendingDataObj.shelfBoxCoords,
+          shelfGridCoords: shelfBoxSendingDataObj.shelfGridCoords,
+        });
+      });
+    })
+    .catch((error) => {
+      console.error('Error adding document: ', error);
+      // alert('Error adding document: ', error.message);
+    });
 }
