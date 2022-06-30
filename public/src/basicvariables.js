be3204b9fdffeeb93a8d5ffc7ca27e598959a777
bagotaj <bagotaj@gmail.com>
2022-06-30 19:53:40 +0200

    Adding new listOfImages elements to array. Adding setClickImageuploadCanvas and setUploadImageupload variables

--- public/src/basicvariables.js
+++ public/src/basicvariables.js
@@ -1,14 +1,23 @@
 let savedBasicsShelvesData = [];
-let setClickImageuploadCanvas = 'grid';
+let setClickImageuploadCanvas;
+let setUploadImageupload;
 let backgroundShelvesWidth;
 let backgroundShelvesHeight;
 let backgroundShelvesTitle;
+// Index uses it
+// let backgroundShelvesWidth = 1359;
+// Edit and Index use it
+// let backgroundShelvesHeight = 500;
+// Index uses it
+// let backgroundShelvesTitle = 'kelet-header';
 const canvasTop = document.getElementById('canvas');
 let imgNewSizeRatio = 6.048;
 let imgSizeRatioBookTitle = 0;
 let shelvesCoordsRatio = 1;
 const listOfImages = [
   { column: 1, images: ['IMG_3365', 'IMG_3366', 'IMG_3367'] },
+  { column: 2, images: ['IMG_3868', 'IMG_3869', 'IMG_3870', 'IMG_3871'] },
+  { column: 3, images: ['IMG_3872', 'IMG_3873', 'IMG_3874', 'IMG_3875'] },
   { column: 4, images: ['IMG_3424', 'IMG_3425', 'IMG_3426'] },
   { column: 5, images: ['IMG_3421', 'IMG_3422', 'IMG_3423'] },
   { column: 6, images: ['IMG_3414', 'IMG_3415', 'IMG_3427', 'IMG_3428'] },
