c6f0915361f57b20169154285ff1ffa9635fc576
bagotaj <bagotaj@gmail.com>
2022-07-02 11:02:53 +0200

    Adding new image file names to listOfImages and declaring resizedLocalImageFile variable

--- public/src/basicvariables.js
+++ public/src/basicvariables.js
@@ -1,4 +1,5 @@
 let savedBasicsShelvesData = [];
+let resizedLocalImageFile;
 let setClickImageuploadCanvas;
 let setUploadImageupload;
 let backgroundShelvesWidth;
@@ -21,7 +22,7 @@ const listOfImages = [
   { column: 4, images: ['IMG_3424', 'IMG_3425', 'IMG_3426'] },
   { column: 5, images: ['IMG_3421', 'IMG_3422', 'IMG_3423'] },
   { column: 6, images: ['IMG_3414', 'IMG_3415', 'IMG_3427', 'IMG_3428'] },
-  { column: 7, images: ['IMG_3785', 'IMG_3787'] },
+  { column: 7, images: ['IMG_3785', 'IMG_3787', 'IMG_3893', 'IMG_3894'] },
   { column: 8, images: ['IMG_3788', 'IMG_3789'] },
 ];
 const numberPerPage = 50;
