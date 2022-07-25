26d076ba1c4db859bb865dd8ef7e4b34ed0121d9
bagotaj <bagotaj@gmail.com>
2022-07-25 20:26:21 +0200

    Adding dataIdCounter variable and dataset.id to shelfButton

--- public/src/makeStartImageUploadPageButtons.js
+++ public/src/makeStartImageUploadPageButtons.js
@@ -1,15 +1,13 @@
-function makeStartPageButtonsImageUpload(shelfData) {
-  let buttonsPlace = document.querySelector('#shelvesButtons');
-  while (buttonsPlace.firstChild) {
-    buttonsPlace.removeChild(buttonsPlace.firstChild);
-  }
+let dataIdCounter = 0;
 
+function makeStartPageButtonsImageUpload(shelfData) {
   let shelfButton = document.createElement('button');
 
   shelfButton.innerText = shelfData.basicShelfTitle;
   shelfButton.setAttribute('type', 'button');
   shelfButton.setAttribute('class', 'normalbtn');
-  shelfButton.addEventListener('click', () => {
+  shelfButton.dataset.id = dataIdCounter;
+  shelfButton.addEventListener('click', (e) => {
     setClickImageuploadCanvas = 'grid';
 
     canvasImageUpload.classList.remove('displaynone');
@@ -19,12 +17,14 @@ function makeStartPageButtonsImageUpload(shelfData) {
 
     setBackgroundShelvesVariables(shelfData);
 
-    let url = `../public/assets/img/kelet-header.jpeg`;
-    setCanvasWrapperIndex(imageuploadcanvas, url);
-    // getImage(imageuploadcanvas, backgroundShelvesTitle);
+    // let url = `../public/assets/img/kelet-header.jpeg`;
+    // setCanvasWrapperIndex(imageuploadcanvas, url);
+    getImage(imageuploadcanvas, backgroundShelvesTitle);
     // till I use url and setCanvas
-    makeShelfGridFromCoords();
+    // makeShelfGridFromCoords();
   });
 
   buttonsPlace.appendChild(shelfButton);
+
+  dataIdCounter = dataIdCounter + 1;
 }
