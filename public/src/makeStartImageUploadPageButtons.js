063cfff472dacf981f539f0b0862fa467680d84d
bagotaj <bagotaj@gmail.com>
2022-07-26 19:38:45 +0200

    Adding condition and canvas selector

--- public/src/makeStartImageUploadPageButtons.js
+++ public/src/makeStartImageUploadPageButtons.js
@@ -9,19 +9,26 @@ function makeStartPageButtonsImageUpload(shelfData) {
   shelfButton.dataset.id = dataIdCounter;
   shelfButton.addEventListener('click', (e) => {
     setClickImageuploadCanvas = 'grid';
-
-    canvasImageUpload.classList.remove('displaynone');
-    showingImageImageUpload.classList.add('displaynone');
-    startPageButtons.classList.add('displaynone');
-    addNewShelfImageButtons.classList.remove('displaynone');
+    let whichCanvas = document.querySelector('canvas');
 
     setBackgroundShelvesVariables(shelfData);
 
-    // let url = `../public/assets/img/kelet-header.jpeg`;
-    // setCanvasWrapperIndex(imageuploadcanvas, url);
-    getImage(imageuploadcanvas, backgroundShelvesTitle);
-    // till I use url and setCanvas
-    // makeShelfGridFromCoords();
+    if (whichCanvas.id === 'canvasEdit') {
+      getImage(canvasEdit, backgroundShelvesTitle);
+    }
+
+    if (whichCanvas.id === 'imageuploadcanvas') {
+      canvasImageUpload.classList.remove('displaynone');
+      showingImageImageUpload.classList.add('displaynone');
+      startPageButtons.classList.add('displaynone');
+      addNewShelfImageButtons.classList.remove('displaynone');
+
+      // let url = `../public/assets/img/kelet-header.jpeg`;
+      // setCanvasWrapperIndex(imageuploadcanvas, url);
+      getImage(imageuploadcanvas, backgroundShelvesTitle);
+      // till I use url and setCanvas
+      // makeShelfGridFromCoords();
+    }
   });
 
   buttonsPlace.appendChild(shelfButton);
