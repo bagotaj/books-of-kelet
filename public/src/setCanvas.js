0d1fbe542aec59cc3a271a15820a0639795f2392
bagotaj <bagotaj@gmail.com>
2022-08-09 20:38:30 +0200

    Adding new canvasWrapperIndexClassWidth from window visual viewport width. Adding isEmptyBasic and isEmptyBook conditions. Declering and defining img variable. Adding different conditions to defining canvasDOMHeight and canvasDOMWidth variables.

--- public/src/setCanvas.js
+++ public/src/setCanvas.js
@@ -6,8 +6,20 @@ function setCanvasHeightIndex(imageWidth) {
 }
 
 function setCanvasWrapperIndex(canvasDOM, imageURL, ImgName) {
-  const isEmpty = Object.keys(backgroundBookShelfImage).length === 0;
-  if (isEmpty) {
+  const isEmptyBasic = Object.keys(backgroundBasicShelfImage).length === 0;
+  const isEmptyBook = Object.keys(backgroundBookShelfImage).length === 0;
+
+  if (canvasDOM.id == 'canvas') {
+    if (isEmptyBasic) {
+      backgroundBasicShelfImage = {
+        canvasDOM: canvasDOM,
+        imageURL: imageURL,
+        ImgName: ImgName,
+      };
+    }
+  }
+
+  if (isEmptyBook) {
     backgroundBookShelfImage = {
       canvasDOM: canvasDOM,
       imageURL: imageURL,
@@ -16,7 +28,6 @@ function setCanvasWrapperIndex(canvasDOM, imageURL, ImgName) {
   }
   let canvasWrapperIndexClass;
   let canvasWrapperIndexClassWidth;
-  console.log('image size ratio', imgSizeRatioBookTitle);
 
   if (canvasDOM.id == 'canvasBookTitles') {
     canvasWrapperIndexClass = document.getElementById('book-shelves-id');
@@ -25,47 +36,55 @@ function setCanvasWrapperIndex(canvasDOM, imageURL, ImgName) {
     canvasWrapperIndexClass = document.querySelectorAll(
       '.canvas-wrapper-index'
     )[0];
-    canvasWrapperIndexClassWidth = canvasWrapperIndexClass.offsetWidth;
+    // canvasWrapperIndexClassWidth = window.innerWidth;
+    canvasWrapperIndexClassWidth = window.visualViewport.width;
   }
 
-  img = new Image();
+  let img = new Image();
   img.src = imageURL;
   img.onload = function () {
     URL.revokeObjectURL(this.src);
 
     let imageWidth = img.width;
     let imageHeight = img.height;
+
     let canvasDOMHeight;
     let canvasDOMWidth;
 
-    if (imageWidth > canvasWrapperIndexClassWidth) {
-      canvasDOMWidth = canvasWrapperIndexClassWidth;
-      canvasDOMHeight =
-        (canvasWrapperIndexClassWidth * imageHeight) / imageWidth;
-    } else if (canvasDOM.id == 'canvasBookTitles') {
+    if (canvasDOM.id == 'canvasBookTitles') {
       let canvasWrapperIndexDOM = document.querySelectorAll(
         '.canvas-wrapper-index'
       )[0];
       canvasDOMHeight = canvasWrapperIndexDOM.offsetHeight * 0.8;
       canvasDOMWidth = (imageWidth * canvasDOMHeight) / imageHeight;
     } else {
-      canvasDOMHeight = imageHeight;
-      canvasDOMWidth = imageWidth;
+      if (imageWidth > canvasWrapperIndexClassWidth) {
+        canvasDOMWidth = canvasWrapperIndexClassWidth;
+        canvasDOMHeight =
+          (canvasWrapperIndexClassWidth * imageHeight) / imageWidth;
+      }
+
+      if (imageWidth < canvasWrapperIndexClassWidth) {
+        canvasDOMHeight = imageHeight;
+        canvasDOMWidth = imageWidth;
+      }
+
+      if (
+        imageWidth < canvasWrapperIndexClassWidth &&
+        canvasDOM.id === 'canvasEdit'
+      ) {
+        canvasDOMHeight = imageHeight;
+        canvasDOMWidth = imageWidth;
+      }
+
+      imgSizeRatioBookTitle =
+        7.56 / ((canvasDOMHeight * 0.8) / (imageHeight * 0.8));
+      shelvesCoordsRatio = canvasDOMHeight / imageHeight;
     }
 
     canvasDOM.height = canvasDOMHeight;
     canvasDOM.width = canvasDOMWidth;
 
-    if (canvasDOM.id !== 'canvasBookTitles') {
-      // console.log(
-      //   'set img size ratio book title',
-      //   canvasDOMHeight,
-      //   imageHeight
-      // );
-      imgSizeRatioBookTitle =
-        7.56 / ((canvasDOMHeight * 0.8) / (imageHeight * 0.8));
-      shelvesCoordsRatio = canvasDOMHeight / imageHeight;
-    }
     canvasWrapperIndexClass.style.height = canvasDOMHeight;
     canvasWrapperIndexClass.style.width = canvasDOMWidth;
     canvasWrapperIndexClass.style.backgroundImage = `url(${imageURL})`;
@@ -81,6 +100,7 @@ function setCanvasWrapperIndex(canvasDOM, imageURL, ImgName) {
 
     if (canvasDOM.id === 'canvasEdit') {
       if (booksFromLocalStorageBoolean) {
+        console.log('imgNewSizeRatio', imgNewSizeRatio);
         drawBoundingBoxes(imgNewSizeRatio, ImgName);
         createBookTitlesTable();
       } else {
