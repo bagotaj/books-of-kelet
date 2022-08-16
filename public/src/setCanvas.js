e8555fa6debf87bbb864f824cbaba16cc78e2389
bagotaj <bagotaj@gmail.com>
2022-08-16 19:27:12 +0200

    Defining and declaring  objects to send them to makeImage function replacing original image making

--- public/src/setCanvas.js
+++ public/src/setCanvas.js
@@ -40,78 +40,80 @@ function setCanvasWrapperIndex(canvasDOM, imageURL, ImgName) {
     canvasWrapperIndexClassWidth = window.visualViewport.width;
   }
 
-  let img = new Image();
-  img.src = imageURL;
-  img.onload = function () {
-    URL.revokeObjectURL(this.src);
+  let sendedObjData = {
+    function: function () {
+      URL.revokeObjectURL(this.src);
 
-    let imageWidth = img.width;
-    let imageHeight = img.height;
+      let imageWidth = img.width;
+      let imageHeight = img.height;
 
-    let canvasDOMHeight;
-    let canvasDOMWidth;
+      let canvasDOMHeight;
+      let canvasDOMWidth;
 
-    if (canvasDOM.id == 'canvasBookTitles') {
-      let canvasWrapperIndexDOM = document.querySelectorAll(
-        '.canvas-wrapper-index'
-      )[0];
-      canvasDOMHeight = canvasWrapperIndexDOM.offsetHeight * 0.8;
-      canvasDOMWidth = (imageWidth * canvasDOMHeight) / imageHeight;
-    } else {
-      if (imageWidth > canvasWrapperIndexClassWidth) {
-        canvasDOMWidth = canvasWrapperIndexClassWidth;
-        canvasDOMHeight =
-          (canvasWrapperIndexClassWidth * imageHeight) / imageWidth;
-      }
-
-      if (imageWidth < canvasWrapperIndexClassWidth) {
-        canvasDOMHeight = imageHeight;
-        canvasDOMWidth = imageWidth;
-      }
-
-      if (
-        imageWidth < canvasWrapperIndexClassWidth &&
-        canvasDOM.id === 'canvasEdit'
-      ) {
-        canvasDOMHeight = imageHeight;
-        canvasDOMWidth = imageWidth;
-      }
-
-      imgSizeRatioBookTitle =
-        7.56 / ((canvasDOMHeight * 0.8) / (imageHeight * 0.8));
-      shelvesCoordsRatio = canvasDOMHeight / imageHeight;
-    }
-
-    canvasDOM.height = canvasDOMHeight;
-    canvasDOM.width = canvasDOMWidth;
-
-    canvasWrapperIndexClass.style.height = canvasDOMHeight;
-    canvasWrapperIndexClass.style.width = canvasDOMWidth;
-    canvasWrapperIndexClass.style.backgroundImage = `url(${imageURL})`;
-    canvasWrapperIndexClass.style.backgroundPosition = 'center';
-
-    if (canvasDOM.id === 'imageuploadcanvas') {
-      makeShelfGridFromCoords({
-        ImgName: ImgName,
-        passedctx: ctxImageUpload,
-        passedcanvas: imageuploadcanvas,
-      });
-    }
-
-    if (canvasDOM.id === 'canvasEdit') {
-      if (booksFromLocalStorageBoolean) {
-        console.log('imgNewSizeRatio', imgNewSizeRatio);
-        drawBoundingBoxes(imgNewSizeRatio, ImgName);
-        createBookTitlesTable();
+      if (canvasDOM.id == 'canvasBookTitles') {
+        let canvasWrapperIndexDOM = document.querySelectorAll(
+          '.canvas-wrapper-index'
+        )[0];
+        canvasDOMHeight = canvasWrapperIndexDOM.offsetHeight * 0.8;
+        canvasDOMWidth = (imageWidth * canvasDOMHeight) / imageHeight;
       } else {
+        if (imageWidth > canvasWrapperIndexClassWidth) {
+          canvasDOMWidth = canvasWrapperIndexClassWidth;
+          canvasDOMHeight =
+            (canvasWrapperIndexClassWidth * imageHeight) / imageWidth;
+        }
+
+        if (imageWidth < canvasWrapperIndexClassWidth) {
+          canvasDOMHeight = imageHeight;
+          canvasDOMWidth = imageWidth;
+        }
+
+        if (
+          imageWidth < canvasWrapperIndexClassWidth &&
+          canvasDOM.id === 'canvasEdit'
+        ) {
+          canvasDOMHeight = imageHeight;
+          canvasDOMWidth = imageWidth;
+        }
+
+        imgSizeRatioBookTitle =
+          7.56 / ((canvasDOMHeight * 0.8) / (imageHeight * 0.8));
+        shelvesCoordsRatio = canvasDOMHeight / imageHeight;
+      }
+
+      canvasDOM.height = canvasDOMHeight;
+      canvasDOM.width = canvasDOMWidth;
+
+      canvasWrapperIndexClass.style.height = canvasDOMHeight;
+      canvasWrapperIndexClass.style.width = canvasDOMWidth;
+      canvasWrapperIndexClass.style.backgroundImage = `url(${imageURL})`;
+      canvasWrapperIndexClass.style.backgroundPosition = 'center';
+
+      if (canvasDOM.id === 'imageuploadcanvas') {
         makeShelfGridFromCoords({
           ImgName: ImgName,
-          passedctx: ctxEdit,
-          passedcanvas: canvasEdit,
+          passedctx: ctxImageUpload,
+          passedcanvas: imageuploadcanvas,
         });
       }
-    }
+
+      if (canvasDOM.id === 'canvasEdit') {
+        if (booksFromLocalStorageBoolean) {
+          drawBoundingBoxes(imgNewSizeRatio, ImgName);
+          createBookTitlesTable();
+        } else {
+          makeShelfGridFromCoords({
+            ImgName: ImgName,
+            passedctx: ctxEdit,
+            passedcanvas: canvasEdit,
+          });
+        }
+      }
+    },
+    src: imageURL,
   };
+
+  let img = makeImage(sendedObjData);
 }
 
 function setImageuploadCanvasBackground(sendingData) {
@@ -124,63 +126,66 @@ function setImageuploadCanvasBackground(sendingData) {
       .removeAttribute('style');
   }
 
-  img = new Image();
-  img.src = sendingData.blobURL;
-  img.onload = function () {
-    URL.revokeObjectURL(this.src);
+  let sendedObjData = {
+    function: function () {
+      URL.revokeObjectURL(this.src);
 
-    if (sendingData.buttonId === 'selectButtonNewBasicShelf') {
-      let dataOfImage = {
-        basicShelfParams: { width: img.width, height: img.height },
-        basicShelfTitle: sendingData.ImgName,
-      };
+      if (sendingData.buttonId === 'selectButtonNewBasicShelf') {
+        let dataOfImage = {
+          basicShelfParams: { width: img.width, height: img.height },
+          basicShelfTitle: sendingData.ImgName,
+        };
 
-      setBackgroundShelvesVariables(dataOfImage);
-    }
+        setBackgroundShelvesVariables(dataOfImage);
+      }
 
-    if (
-      sendingData.buttonId === 'uploadButton' &&
-      setUploadImageupload === 'basicshelf'
-    ) {
-      backgroundShelvesTitle = sendingData.ImgName;
+      if (
+        sendingData.buttonId === 'uploadButton' &&
+        setUploadImageupload === 'basicshelf'
+      ) {
+        backgroundShelvesTitle = sendingData.ImgName;
 
-      let dataOfImageToFirebase = {
-        basicShelfParams: { width: img.width, height: img.height },
-        basicShelfTitle: sendingData.ImgName,
-      };
+        let dataOfImageToFirebase = {
+          basicShelfParams: { width: img.width, height: img.height },
+          basicShelfTitle: sendingData.ImgName,
+        };
 
-      addBasicsToFirestore(dataOfImageToFirebase);
-    }
+        addBasicsToFirestore(dataOfImageToFirebase);
+      }
 
-    if (sendingData.buttonId === 'imageuploadcanvas') {
-      let imageHeightData = {
-        original: img.height,
-        resized: backgroundShelvesHeight,
-      };
+      if (sendingData.buttonId === 'imageuploadcanvas') {
+        let imageHeightData = {
+          original: img.height,
+          resized: backgroundShelvesHeight,
+        };
 
-      imgNewSizeRatio = makeImgNewSizeRatioNumber(imageHeightData);
+        imgNewSizeRatio = makeImgNewSizeRatioNumber(imageHeightData);
 
-      canvas.height = 500;
-      canvas.width = (img.width * 500) / img.height;
+        canvas.height = 500;
+        canvas.width = (img.width * 500) / img.height;
 
-      canvasctx.clearRect(0, 0, canvas.width, canvas.height);
-      canvasctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+        canvasctx.clearRect(0, 0, canvas.width, canvas.height);
+        canvasctx.drawImage(img, 0, 0, canvas.width, canvas.height);
 
-      const mime_type = 'image/jpeg';
+        const mime_type = 'image/jpeg';
 
-      canvas.toBlob((blob) => {
-        resizedLocalImageFile = new File(
-          [blob],
-          `${sendingData.ImgName}.jpg`,
-          { type: 'image/jpeg', lastModified: new Date().getTime() },
-          'utf-8'
-        );
-      }, mime_type);
-    }
+        canvas.toBlob((blob) => {
+          resizedLocalImageFile = new File(
+            [blob],
+            `${sendingData.ImgName}.jpg`,
+            { type: 'image/jpeg', lastModified: new Date().getTime() },
+            'utf-8'
+          );
+        }, mime_type);
+      }
 
-    if (sendingData.buttonId === 'selectButtonNewBasicShelf') {
-      gridXCoords.push(0, canvas.width);
-      gridYCoords.push(0, canvas.height);
-    }
+      if (sendingData.buttonId === 'selectButtonNewBasicShelf') {
+        gridXCoords.push(0, canvas.width);
+        gridYCoords.push(0, canvas.height);
+      }
+    },
+    src: sendingData.blobURL,
   };
+
+  let img = makeImage(sendedObjData);
 }
