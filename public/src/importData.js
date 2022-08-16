71cec47a393ddb390f11752a3c0475708e14976a
bagotaj <bagotaj@gmail.com>
2022-08-16 19:22:55 +0200

    Deleting unused code part

--- public/src/importData.js
+++ public/src/importData.js
@@ -27,7 +27,7 @@ function submitImportData() {
   let docRef = dbconnection.collection(selectedCollection);
 
   for (let batch in selectedData) {
-    //console.log(selectedData[batch]);
+    // console.log(selectedData[batch]);
     selectedData[batch].forEach((book) => {
       docRef
         .add(book)
@@ -40,25 +40,4 @@ function submitImportData() {
         });
     });
   }
-  // console.log(selectedData);
-  // let firebaseBatch = dbconnection.batch();
-
-  // for (let batch in selectedData) {
-  //   console.log('Batch', selectedData[batch].length);
-  //   selectedData[batch].forEach((book) => {
-  //     let docRef = dbconnection.collection(selectedCollection).doc();
-
-  //     firebaseBatch.set(docRef, book);
-  //   });
-  // }
-
-  // firebaseBatch
-  //   .commit()
-  //   .then(() => {
-  //     console.log('Document written');
-  //   })
-  //   .catch((error) => {
-  //     console.error('Error adding document: ', error);
-  //     alert('Error adding document: ', error.message);
-  //   });
 }
